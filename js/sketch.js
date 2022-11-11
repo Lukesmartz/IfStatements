813908a0b7e71a0c443c9fae3a1381eea13cc815
Lukesmartz <116882858+Lukesmartz@users.noreply.github.com>
2022-11-11 13:03:29 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,45 @@
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(400, 400);
     canvas.parent("p5container");
 }
+
+
 
 function draw() {
     background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    if (mouseX < width/2){
+    // console.log('LEFT') //wrie something in console
+    fill( 238, 242, 9)
+    circle(200, 200, 60);
+
+    fill( 0)
+    circle(190, 190, 5);
+
+    fill( 0)
+    circle(210, 190, 5); 
+
+    fill (0)
+    arc(200, 210, 25, 25, 25,  PI, TWO_PI );
+
+    }
+    
+    if (mouseX > width/2){
+        fill( 242, 9, 9)
+        circle(200, 200, 60);
+    
+        fill( 0)
+        circle(190, 190, 5);
+    
+        fill( 0)
+        circle(210, 190, 5); 
+
+      fill (0)
+    arc(200, 210, 25, 25, 25,  PI, TWO_PI );
+    // console.log('RIGHT') //wrie something in console
+    
+    // text('RIGHT', 200, 200)
+    } 
+    // fill(255, 255, 255);
+    // ellipse(400, 400, 200);
 }
